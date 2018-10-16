fb6e1ba26592f33296241a804662da05bb913674
NgoQuocDat <shaco14091999@gmail.com>
2018-10-16 17:38:21 +0700

    add

---
+++ LeapYear/src/leapyear/LeapYear.java
@@ -0,0 +1,33 @@
+package leapyear;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        int year;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        year=sc.nextInt();
+        boolean isLeapYear = false;
+
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if(isDivisibleBy4){
+        boolean isDivisibleBy100 = year % 100 == 0;
+        if(isDivisibleBy100){
+            boolean isDivisibleBy400 = year % 400 == 0;
+            if(isDivisibleBy400){
+                isLeapYear = true;
+            }
+        } else {
+            isLeapYear = true;
+        }
+    }
+
+    if(isLeapYear){
+        System.out.printf("%d is a leap year", year);
+    } else {
+        System.out.printf("%d is NOT a leap year", year);
+    }
+    }   
+}
